14570c4b92375892aa925ab083e965d792ddf112
Luizgusta <52894014+Luizgusta@users.noreply.github.com>
2026-09-26 13:17:37 -0300

    Update image path for Seda Papelito Brown King Size

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -21,7 +21,7 @@ const categories = [
 
 const products = [
   { name: "Piteira Alameda Large", category: "Acessórios", price: "R$ 8,00", oldPrice: "", image: "./images/produto-seda-acrema.jpg", tag: "Alameda" },
-  { name: "Seda Papelito Brown King Size", category: "Sedas", price: "R$ 4,00", oldPrice: "", image: "./images/produto-papelitos.jpg", tag: "Essencial" },
+  { name: "Seda Papelito Brown King Size", category: "Sedas", price: "R$ 4,00", oldPrice: "", image: "./images/papelitos.jpg", tag: "Essencial" },
   { name: "Tabaco Santorini 25g", category: "Tabacos", price: "R$ 25,00", oldPrice: "", image: "./images/alameda-category-tabaco.jpg", tag: "Curadoria" },
   { name: "Kit Brown + Piteira", category: "Combos", price: "R$ 7,00", oldPrice: "R$ 9,00", image: "./images/alameda-category-sedas.jpg", tag: "Oferta" },
 ];
